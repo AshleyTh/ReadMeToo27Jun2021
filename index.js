450fc15b905aa77ea49a28fba6e918113441079a
AshleyTh <80226781+AshleyTh@users.noreply.github.com>
2021-10-04 18:49:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function writeToFile(data) {
+    fs.writeFile('./readme/readme.md', data, function (err) {
+        if (err) return console.log(err);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Title?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Description?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Installation?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Usage?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'License?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Contributing?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Test?',
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Questions?',
+            },
+        ])
+        .then((answer) => {
+            var input = generatePage(answer)
+            writeToFile(input)
+        })
+}
 // Function call to initialize app
 init();
